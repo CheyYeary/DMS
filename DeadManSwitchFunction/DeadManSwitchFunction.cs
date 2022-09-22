fbd519df764bdecc726625b58086e85e5c664523
cheyyeary <cheyyeary@microsoft.com>
2022-09-22 12:15:52 -0700

    added email send super basic

--- DeadManSwitchFunction/DeadManSwitchFunction.cs
+++ DeadManSwitchFunction/DeadManSwitchFunction.cs
@@ -13,6 +13,12 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using FluentEmail.Smtp;
+using FluentEmail.Core;
+using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -33,6 +39,32 @@ namespace DeadManSwitchFunction
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com");
+
+            var _sender = "deadmanswitchhackathon@outlook.com";
+            var _password = "";
+
+
+            System.Net.NetworkCredential creds = new System.Net.NetworkCredential(_sender, _password);
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = creds;
+
+
+            var sender = new SmtpSender(smtp);
+            var time = DateTime.Now.ToString("yyyyMMddHHmmss");
+            Email.DefaultSender = sender;
+
+            var email = await Email
+                .From("deadmanswitchhackathon@outlook.com")
+                .To("cheyj.yeary@gmail.com", "Chey")
+                .Subject($"DMS has been activated number {time}")
+                .Body("DMS from one of our users has been invoked and you have been placed on a list to recieve files from the user.")
+                .SendAsync();
+
+            Console.WriteLine("Successful send: " + email.Successful);
 
             await this.Initialize();
 
---
+++ DeadManSwitchFunction/README.md
@@ -0,0 +1,6 @@
+
+Get azure functions to run azure function code locally. 
+https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Cmacos%2Ccsharp%2Cportal%2Cbash#v2
+
+
+RUN: func start --csharp
